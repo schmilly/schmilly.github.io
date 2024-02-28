bd19982030c95413df387e62e9742c0860cfec33
schmilly <schmilly@proton.me>
2024-02-28 13:02:23 +0800

    Included new EMRS data

--- CoalitonCalculator/polling.js
+++ CoalitonCalculator/polling.js
@@ -99,7 +99,7 @@ var Result_2021 = [
 {...Electorates[ElectID.Clark], PrimaryVote: [31.8,22.1,20,20.8,5.3], Seats:[1,2,1,0,1]},
 {...Electorates[ElectID.Franklin], PrimaryVote: [42.3,33.2,18.9,0.5,5.1]},
 {...Electorates[ElectID.Bass], PrimaryVote: [31.8,22.1,20,20.8,5.3]},
-{...Electorates[ElectID.Lyons], PrimaryVote: [51.2,32.5,8.9,0.9,1.2]},
+{...Electorates[ElectID.Lyons], PrimaryVote: [51.2,32.5,8.9,0.9,6.5]},
 {...Electorates[ElectID.Braddon], PrimaryVote: [57.2,26.5,5.5,6.1,4.7]},
 ]  
 
@@ -133,7 +133,21 @@ const TasmaniaState =
         "SeatMax": 35,
         "ElectorateSeatCount": 7,
         "Electorate": Result_2021,
-    },
+        },
+        {
+            "Date": "2024-02-21",
+            "Company": "EMRS Poll",
+            "Level": "Tasmanian State 2024-02-21 EMRS Poll",
+            "Count": 6,
+            "PartyLabels": [PrtyNme.Lib, PrtyNme.ALP, PrtyNme.Grn, PrtyNme.JLN, PrtyNme.Ind, PrtyNme.Oth],
+            "PartyData": [39, 26, 12, 9, 14, 1],
+            "PartyColor": [PartyColor.Lib, PartyColor.ALP, PartyColor.Grn, PartyColor.JLN, PartyColor.Ind, PartyColor.Oth],
+            "SeatProj": [15, 11, 3, 6, 0, 0],
+            "SeatMajor": 18,
+            "SeatMax": 35,
+            "ElectorateSeatCount": 7,
+            "Electorate": Result_2021,
+        },
     {
         "Date": "2024-01-10",
         "Company": "You Gov Poll",
