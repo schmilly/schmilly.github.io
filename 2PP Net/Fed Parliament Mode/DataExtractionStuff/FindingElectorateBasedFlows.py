089cc5c6afd2da5456951f351fd05cb9ea791c07
schmilly <william.f.s.bake@gmail.com>
2024-09-09 23:51:23 +0800

    Changed over some data and did some changes

--- 2PP Net/Fed Parliament Mode/DataExtractionStuff/FindingElectorateBasedFlows.py
+++ 2PP Net/Fed Parliament Mode/DataExtractionStuff/FindingElectorateBasedFlows.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 # Step 1: Read the CSV file
-df = pd.read_csv('yourfile.csv')
+df = pd.read_csv('Electorate_by_Electorate_flows-With-Electorates.csv')
 
 # Step 2: Create the lookup table
-lookup_table = df.groupby('Column1')['Column2'].unique().reset_index()
+lookup_table = df.groupby('Electorate')['ID1']['ID2'].unique().reset_index()
 
 # If you want it to be a dictionary
-lookup_dict = lookup_table.set_index('Column1')['Column2'].to_dict()
+lookup_dict = lookup_table.set_index('Electorate')['ID1']['ID2'].to_dict()
 
 # Step 3: Save the lookup table to a new CSV file
 lookup_table.to_csv('lookup_table.csv', index=False)
